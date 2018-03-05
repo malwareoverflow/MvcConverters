1791e5813cff192afa51a5f85d84986ddf3b984c
Taha Temuri <tahatemuri7@outlook.com>
2018-03-05 12:49:19 +0500

    pdf ti word class code changes

--- MvcConverters/ConvertersTypes/PdftoWord.cs
+++ MvcConverters/ConvertersTypes/PdftoWord.cs
@@ -18,59 +18,54 @@ namespace MvcConverters.ConvertersTypes
         public string ContentType { get; set; } = "application/docx";
         public override MemoryStream Convert()
         {
-            PDDocument doc = null;
-            var stream = new MemoryStream();
-            var fileName = Path.GetFileName(File.FileName);
-            string newfilename = DateTime.Now.ToString("yyyyMMdd_hhmmss");
-          
-            File.SaveAs(Path.Combine(System.Web.HttpContext.Current.Server.MapPath(@"~/test/"),$"{newfilename}.pdf"));
-            doc = PDDocument.load(System.Web.HttpContext.Current.Server.MapPath($"~/test/{newfilename}.pdf"));
-            PDFTextStripper textStrip = new PDFTextStripper();
-            string PdfText = textStrip.getText(doc);
-            doc.close();
 
-            string wordsavePath = System.Web.HttpContext.Current.Server.MapPath($"~/test/{newfilename}.docx");
-            var wordDoc = DocX.Create(wordsavePath);
-            wordDoc.InsertParagraph(PdfText);
-            wordDoc.Save();
-            System.Diagnostics.Process.Start("WINWORD.EXE", wordsavePath);
+       
+            //PDDocument doc = null;
+            //var stream = new MemoryStream();
+            //var fileName = Path.GetFileName(File.FileName);
+            string newfilename = DateTime.Now.ToString("yyyyMMdd_hhmmss");
+
+            File.SaveAs(Path.Combine(System.Web.HttpContext.Current.Server.MapPath(@"~/test/"),$"{newfilename}.pdf"));
+            string pdfFile = System.Web.HttpContext.Current.Server.MapPath($"~/test/{newfilename}.pdf");
+            MemoryStream docxStream = new MemoryStream();
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+
+            using (FileStream pdfStream = new FileStream(pdfFile, FileMode.Open, FileAccess.Read))
+            {
+
+                f.OpenPdf(pdfStream);
+                if (f.PageCount > 0)
+                {
+
+                    int res = f.ToWord(docxStream);
+                    if (res == 0)
+                    {
+
+                        string docxFile = Path.ChangeExtension(pdfFile, ".docx");
+                      System.IO.File.WriteAllBytes(docxFile, docxStream.ToArray());
+                        System.Diagnostics.Process.Start(docxFile);
+                    }
+                }
+            }
+
             using (MemoryStream ms = new MemoryStream())
             using (FileStream file = new FileStream(System.Web.HttpContext.Current.Server.MapPath($"~/test/{newfilename}.docx"), FileMode.Open, FileAccess.Read))
             {
                 byte[] bytes = new byte[file.Length];
                 file.Read(bytes, 0, (int)file.Length);
                 ms.Write(bytes, 0, (int)file.Length);
-                stream = ms;
-                stream.Flush();
-                stream.Close();
+                docxStream = ms;
+                docxStream.Flush();
+                docxStream.Close();
             }
-           
-  
-            return stream;
+
+
+            return docxStream;
+
         }
 
 
 
-        public static byte[] ReadFile(string filePath)
-        {
-            byte[] buffer;
-            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            try
-            {
-                int length = (int)fileStream.Length;  // get file length
-                buffer = new byte[length];            // create buffer
-                int count;                            // actual number of bytes read
-                int sum = 0;                          // total number of bytes read
-
-                // read until Read method returns 0 (end of the stream has been reached)
-                while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
-                    sum += count;  // sum is a buffer offset for next reading
-            }
-            finally
-            {
-                fileStream.Close();
-            }
-            return buffer;
-        }
+     
     }
 }
